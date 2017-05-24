834ea81630aa7b29bd456761dbf9eb12eddc3687
Ryan <kuhnrl30@gmail.com>
2017-05-23 23:35:15 -0400

    upload daily progress 5/23

--- R/LendingClubModel.R
+++ R/LendingClubModel.R
@@ -1,10 +1,14 @@
 #' Lending Club Model
 #'
-#' To document the process to build a model for increasing
+#' To document the process of building a model which increasing
 #' investor returns.
 #'
 #' @docType package
 #' @name LendingClubModel
-#' @author Ryan Kuhn, CPA
+#' @author Ryan Kuhn, CPA, \email{kuhnrl30@gmail.com} 
 #' @import dplyr
+#' @seealso \itemize{
+#' \item{\url{http://ryankuhn.net/LendingClubModel}}
+#' \item{\url{http://github.com/kuhnrl30/LendingClubModel/issues} for bug reports}
+#' }
 NULL
