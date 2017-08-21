f1ee571fbdc7ebdc97484f915ddc0b1479545833
kuhnrl30 <kuhnrl30@gmail.com>
2017-08-21 08:16:12 -0400

    upload progress 8/20 [skip ci]

--- buildsite.R
+++ buildsite.R
@@ -7,3 +7,5 @@ library(rmarkdown)
 source("environment.R")
 source("LoadData.R")
 rmarkdown::render_site(".", output_format = "html_document")
+
+rmarkdown::render("ModelReviewNotes.Rmd", output_format = "pdf_document")
