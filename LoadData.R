419deb32446fbbea2d688a789667c1804e0efdbb
kuhnrl30 <kuhnrl30@gmail.com>
2017-10-03 00:27:57 -0400

    daily progress (10/2)

--- LoadData.R
+++ LoadData.R
@@ -1,3 +1,4 @@
 LoanData<- LendingClubData::IssuedLoans(2007:2017)
 LoanData$Class<- LendingClubData::SetClass(LoanData$loan_status)
 LoanData<- LoanData[!is.na(LoanData$loan_amnt),]
+LoadData<- LoanData %>% filter(policy_code==1) %>% select(-policy_code)
