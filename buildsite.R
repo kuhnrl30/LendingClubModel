175a9d38077f6937e443d071076ed70e2e467537
kuhnrl30 <kuhnrl30@gmail.com>
2017-10-15 00:41:32 -0400

    upload progress 11/14

--- buildsite.R
+++ buildsite.R
@@ -2,15 +2,17 @@
 source("environment.R")
 source("LoadData.R")
 
-rmarkdown::render_site(".", output_format = "html_document")
+# rmarkdown::render_site(".", output_format = "html_document")
 
 
 #Keep getting memory allocation errors so will loop through
 RMD_files<- grep("^(?!_).+Rmd", dir(), value=T, perl=T)
-lapply(RMD_files, function(x) rmarkdown::render_site(x, output="html_document"))
+lapply(RMD_files, function(x) rmarkdown::render_site(x, output_format="html_document"))
+rm(LoanData)
 
 # To clean temp files and recover memory
 #system("sudo  find /tmp -type f -atime +0")
 
 
 rmarkdown::render("ModelReviewNotes.Rmd", output_format = "pdf_document")
+rmarkdown::render("AnnotatedBibliography.Rmd", output_format = "html_document")
